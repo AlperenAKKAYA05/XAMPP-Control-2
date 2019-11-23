f50a204e7501529bba0310ba140f215d16e7318c
Alperen <38250510+Crpsem@users.noreply.github.com>
2019-11-24 01:10:47 +0300

    xampp-control-2
    xampp-control-2

---
+++ xampp-control2/Form1.Designer.vb
@@ -0,0 +1,211 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form, bileşen listesini temizlemeyi bırakmayı geçersiz kılar.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form Tasarımcısı tarafından gerektirilir
+    Private components As System.ComponentModel.IContainer
+
+    'NOT: Aşağıdaki yordam Windows Form Tasarımcısı için gereklidir
+    'Windows Form Tasarımcısı kullanılarak değiştirilebilir.  
+    'Kod düzenleyicisini kullanarak değiştirmeyin.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.FalseApache = New System.Windows.Forms.PictureBox()
+        Me.FalseMySQL = New System.Windows.Forms.PictureBox()
+        Me.TrueApache = New System.Windows.Forms.PictureBox()
+        Me.TrueMySQL = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.NotifyIcon = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.GirişToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.UyarıToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ÇıkışToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ÇıkışToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        CType(Me.FalseApache, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.FalseMySQL, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TrueApache, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TrueMySQL, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(36, 12)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(63, 17)
+        Me.CheckBox1.TabIndex = 3
+        Me.CheckBox1.Text = "Apache"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(36, 35)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(61, 17)
+        Me.CheckBox2.TabIndex = 4
+        Me.CheckBox2.Text = "MySQL"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'FalseApache
+        '
+        Me.FalseApache.Image = Global.xampp_control_2.My.Resources.Resources._False
+        Me.FalseApache.Location = New System.Drawing.Point(94, 2)
+        Me.FalseApache.Name = "FalseApache"
+        Me.FalseApache.Size = New System.Drawing.Size(21, 27)
+        Me.FalseApache.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.FalseApache.TabIndex = 5
+        Me.FalseApache.TabStop = False
+        '
+        'FalseMySQL
+        '
+        Me.FalseMySQL.Image = Global.xampp_control_2.My.Resources.Resources._False
+        Me.FalseMySQL.Location = New System.Drawing.Point(94, 25)
+        Me.FalseMySQL.Name = "FalseMySQL"
+        Me.FalseMySQL.Size = New System.Drawing.Size(21, 27)
+        Me.FalseMySQL.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.FalseMySQL.TabIndex = 6
+        Me.FalseMySQL.TabStop = False
+        '
+        'TrueApache
+        '
+        Me.TrueApache.Image = Global.xampp_control_2.My.Resources.Resources._True
+        Me.TrueApache.Location = New System.Drawing.Point(94, 2)
+        Me.TrueApache.Name = "TrueApache"
+        Me.TrueApache.Size = New System.Drawing.Size(21, 27)
+        Me.TrueApache.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.TrueApache.TabIndex = 7
+        Me.TrueApache.TabStop = False
+        '
+        'TrueMySQL
+        '
+        Me.TrueMySQL.Image = Global.xampp_control_2.My.Resources.Resources._True
+        Me.TrueMySQL.Location = New System.Drawing.Point(94, 25)
+        Me.TrueMySQL.Name = "TrueMySQL"
+        Me.TrueMySQL.Size = New System.Drawing.Size(21, 27)
+        Me.TrueMySQL.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.TrueMySQL.TabIndex = 8
+        Me.TrueMySQL.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(121, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(66, 13)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "Apache | Off"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(121, 35)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(64, 13)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "MySQL | Off"
+        '
+        'NotifyIcon
+        '
+        Me.NotifyIcon.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.NotifyIcon.Icon = CType(resources.GetObject("NotifyIcon.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon.Text = "NotifyIcon"
+        Me.NotifyIcon.Visible = True
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.GirişToolStripMenuItem, Me.UyarıToolStripMenuItem, Me.ÇıkışToolStripMenuItem, Me.ÇıkışToolStripMenuItem1})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(109, 92)
+        '
+        'GirişToolStripMenuItem
+        '
+        Me.GirişToolStripMenuItem.Name = "GirişToolStripMenuItem"
+        Me.GirişToolStripMenuItem.Size = New System.Drawing.Size(108, 22)
+        Me.GirişToolStripMenuItem.Text = "Gizle"
+        '
+        'UyarıToolStripMenuItem
+        '
+        Me.UyarıToolStripMenuItem.Name = "UyarıToolStripMenuItem"
+        Me.UyarıToolStripMenuItem.Size = New System.Drawing.Size(108, 22)
+        Me.UyarıToolStripMenuItem.Text = "Göster"
+        '
+        'ÇıkışToolStripMenuItem
+        '
+        Me.ÇıkışToolStripMenuItem.Name = "ÇıkışToolStripMenuItem"
+        Me.ÇıkışToolStripMenuItem.Size = New System.Drawing.Size(108, 22)
+        Me.ÇıkışToolStripMenuItem.Text = "Uyarı"
+        '
+        'ÇıkışToolStripMenuItem1
+        '
+        Me.ÇıkışToolStripMenuItem1.Name = "ÇıkışToolStripMenuItem1"
+        Me.ÇıkışToolStripMenuItem1.Size = New System.Drawing.Size(108, 22)
+        Me.ÇıkışToolStripMenuItem1.Text = "Çıkış"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(231, 61)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TrueMySQL)
+        Me.Controls.Add(Me.TrueApache)
+        Me.Controls.Add(Me.FalseMySQL)
+        Me.Controls.Add(Me.FalseApache)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(247, 100)
+        Me.MinimizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(247, 100)
+        Me.Name = "Form1"
+        Me.Text = "XAMPP Control 2"
+        CType(Me.FalseApache, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.FalseMySQL, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TrueApache, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TrueMySQL, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Private Sub txtOutput_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents FalseApache As PictureBox
+    Friend WithEvents FalseMySQL As PictureBox
+    Friend WithEvents TrueApache As PictureBox
+    Friend WithEvents TrueMySQL As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Private WithEvents NotifyIcon As NotifyIcon
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents GirişToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents UyarıToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ÇıkışToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ÇıkışToolStripMenuItem1 As ToolStripMenuItem
+End Class
---
+++ xampp-control2/Form1.vb
@@ -0,0 +1,82 @@
+﻿Imports System.Runtime.InteropServices
+Imports System.IO
+Imports System.ComponentModel
+
+Partial Class Form1
+    Dim kapatma = 0
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked = True Then
+            Process.Start(Application.StartupPath & "/apache_start.exe", AppWinStyle.Hide)
+            TrueApache.Visible = True
+            FalseApache.Visible = False
+            Label1.Text = "Apache | On"
+
+
+        End If
+        If CheckBox1.Checked = False Then
+            Process.Start(Application.StartupPath & "/apache_stop.bat", AppWinStyle.Hide)
+            Shell("TASKKILL /F /IM cmd.exe")
+            Shell("TASKKILL /F /IM httpd.exe")
+            Shell("TASKKILL /F /IM conhost.exe")
+            TrueApache.Visible = False
+            FalseApache.Visible = True
+            Label1.Text = "Apache | Off"
+        End If
+    End Sub
+
+    Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
+        If CheckBox2.Checked = True Then
+            Process.Start(Application.StartupPath & "/mysql_start.exe", AppWinStyle.Hide)
+            TrueMySQL.Visible = True
+            FalseMySQL.Visible = False
+            Label2.Text = "MySQL | On"
+        End If
+        If CheckBox2.Checked = False Then
+            Process.Start(Application.StartupPath & "/mysql_stop.bat", AppWinStyle.Hide)
+            Shell("TASKKILL /F /IM cmd.exe")
+            Shell("TASKKILL /F /IM mysqld.exe")
+            TrueMySQL.Visible = False
+            FalseMySQL.Visible = True
+            Label2.Text = "MySQL | Off"
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TrueApache.Visible = False
+        TrueMySQL.Visible = False
+    End Sub
+
+    Private Sub ÇıkışToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ÇıkışToolStripMenuItem.Click
+        NotifyIcon.ShowBalloonTip(10, "Başlık", "Uyarı", ToolTipIcon.Info)
+    End Sub
+
+    Private Sub GirişToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles GirişToolStripMenuItem.Click
+        Me.Hide()
+    End Sub
+
+    Private Sub UyarıToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles UyarıToolStripMenuItem.Click
+        Me.Show()
+    End Sub
+
+    Private Sub ÇıkışToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles ÇıkışToolStripMenuItem1.Click
+        kapatma = 1
+        Close()
+    End Sub
+
+    Private Sub Form1_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        NotifyIcon.ShowBalloonTip(10, "Başlık", "Uyarı", ToolTipIcon.Info)
+        Me.Hide()
+        If kapatma = 1 Then
+            Process.Start(Application.StartupPath & "/apache_stop.bat", AppWinStyle.Hide)
+            Process.Start(Application.StartupPath & "/mysql_stop.bat", AppWinStyle.Hide)
+            Shell("TASKKILL /F /IM cmd.exe")
+            Shell("TASKKILL /F /IM mysqld.exe")
+            Shell("TASKKILL /F /IM httpd.exe")
+            Shell("TASKKILL /F /IM conhost.exe")
+            e.Cancel = False
+        End If
+        If kapatma = 0 Then
+            e.Cancel = True
+        End If
+    End Sub
+End Class
